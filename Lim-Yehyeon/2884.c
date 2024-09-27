74c03303b1a3d5210003968f3aadbb71acb09890
Lim-Yehyeon <yh2269@pusan.ac.kr>
2024-09-27 14:07:14 +0900

    Add files via upload
    입력
    첫째 줄에 두 정수 H와 M이 주어집니다.(0 ≤ H ≤ 23, 0 ≤ M ≤ 59) 현재 설정한 알람 시간 H시 M분을 의미합니다.
    입력 시간은 24시간 표현을 사용합니다. 자정을 0:0으로, 하루의 끝을 23:59라고 합니다. 불필요한 0은 사용하지 않습니다.
    
    출력
    첫째 줄에 설정해야 하는 알람 시간을 출력합니다.

---
+++ Lim-Yehyeon/2884.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int h, m;
+	scanf("%d %d", &h, &m);
+	
+	if(m>=45){
+		printf("%d %d", h, m-45);
+	}
+	else if(h>0){
+		printf("%d %d", h-1, 60-(45-m));
+	}
+	else
+		printf("23 %d", 60-(45-m));
+	
+	return 0;
+}
